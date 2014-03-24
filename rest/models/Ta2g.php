8abd80ff5abdc96ce4d8d1ee0887ee50b3fff716
githubjeka <et.coder@gmail.com>
2014-03-24 16:28:35 +0300

    rename

---
+++ rest/models/Ta2g.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "tbl_tag".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $frequency
+ */
+class Tag extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_tag';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['frequency'], 'integer'],
+            [['name'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'frequency' => 'Frequency',
+        ];
+    }
+}

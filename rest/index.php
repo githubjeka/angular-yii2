c7a8eb78519c032c220d9a9b8b1346c7b488e45b
githubjeka <et.coder@gmail.com>
2014-03-28 16:42:18 +0300

    add header access

--- rest/index.php
+++ rest/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS ');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 ini_set('display_errors', 1);
 
